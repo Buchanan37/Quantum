cf961f167f2cd458fcc10d2aa89913c847f26a79
Buchanan37 <49928194+Buchanan37@users.noreply.github.com>
2022-07-24 23:13:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js")
-
-const TOKEN = "MTAwMDg1ODIyODk0MDM0MTM1OA.GtW_na.u0gi1uIF7TE5JqQoyThJqd33VtE_knnSVTdn7k"
+require("dotenv").config()
 
 const client = new Discord.Client({
     intents: [
